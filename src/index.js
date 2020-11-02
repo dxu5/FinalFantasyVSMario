92b39aea7a899d2fd641499f31027bd24fefbb5e
dxu5 <derek_xu@brown.edu>
2020-11-02 14:20:39 -0800

    Fix rearrange bug with code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Game from "./game_properties/game.js";
 import GameMain from "./game_main.js";
 import Display from "./display/display.js";
+import Controller from "./controller.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   const height = 400;
@@ -8,7 +9,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const canvas = document.getElementById("canvas");
   const game = new Game(height, width);
-  const gameMain = new GameMain(game, display, controller);
   const display = new Display(canvas, height, width);
+  const controller = new Controller(game.world.mario);
+
+  const gameMain = new GameMain(game, display, controller);
+
   gameMain.start();
 });

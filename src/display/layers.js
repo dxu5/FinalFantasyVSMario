1217b8b6039eb95662dfce3d2bd00735eb743aee
dxu5 <derek_xu@brown.edu>
2020-11-02 14:14:28 -0800

    Add display class for game and load assests

---
+++ src/display/layers.js
@@ -0,0 +1,18 @@
+export function createBackgroundLayer(backgroundSheet) {
+  console.log("fetching json");
+  readJSON(`./world.json`);
+}
+function readJSON(path) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", path, true);
+  xhr.responseType = "blob";
+  xhr.onload = function (e) {
+    if (this.status == 200) {
+      var file = new File([this.response], "temp");
+      var fileReader = new FileReader();
+      fileReader.addEventListener("load", function () {});
+      fileReader.readAsText(file);
+    }
+  };
+  xhr.send();
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Game from "./game_properties/game.js";
 import GameMain from "./game_main.js";
+import Display from "./display/display.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   const height = 400;
@@ -8,5 +9,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const canvas = document.getElementById("canvas");
   const game = new Game(height, width);
   const gameMain = new GameMain(game, display, controller);
+  const display = new Display(canvas, height, width);
   gameMain.start();
 });

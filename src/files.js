b63430be09491d5a01961e84bee5c871a350f4c5
dxu5 <derek_xu@brown.edu>
2020-11-02 14:01:28 -0800

    Add assests into project

---
+++ src/files.js
@@ -0,0 +1,8 @@
+import backgroundAssets from "./imgs/background_assets.png";
+import mario from "./imgs/mario.png";
+
+export const backgroundImage = new Image();
+backgroundImage.src = backgroundAssets;
+
+export const marioImage = new Image();
+marioImage.src = mario;

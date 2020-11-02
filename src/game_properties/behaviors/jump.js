31e743c4b46a2bcbd7be57e074df43b803972a22
dxu5 <derek_xu@brown.edu>
2020-11-02 15:56:35 -0800

    Add ground properties to mario

--- src/game_properties/behaviors/jump.js
+++ src/game_properties/behaviors/jump.js
@@ -3,15 +3,17 @@ import Behavior from "./behavior";
 export default class Jump extends Behavior {
   constructor() {
     super("jump");
-    this.maxDuration = 0.3;
+    this.maxDuration = 0.25;
     this.vel = 300;
     this.duration = 0;
+    this.isGrounded = true;
   }
 
   start() {
-    this.duration = this.maxDuration;
+    if (this.isGrounded) this.duration = this.maxDuration;
   }
   cancel() {
+    this.isGrounded = false;
     this.duration = 0;
   }
 
@@ -19,6 +21,9 @@ export default class Jump extends Behavior {
     if (this.duration > 0) {
       mario.vel.y = -this.vel;
       this.duration -= deltaTime;
+    }
+    if (mario.isGrounded) {
+      this.isGrounded = true;
     }
   }
 }

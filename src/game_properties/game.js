6fa93a3a577fcfdd65aed335c018ceda546fe0aa
dxu5 <derek_xu@brown.edu>
2020-11-03 09:09:03 -0800

    Add camera view function"

--- src/game_properties/game.js
+++ src/game_properties/game.js
@@ -15,6 +15,7 @@ export default class Game {
     this.tileMap = [];
     this.collider = new Collider(this.tileMap);
     this.setTilemapLayer = this.setTilemapLayer.bind(this);
+    this.cameraView = this.cameraView.bind(this);
     this.setTilemapLayer();
   }
   update(deltaTime) {
@@ -29,7 +30,6 @@ export default class Game {
       object.vel.y += this.gravity;
       object.pos.y += object.vel.y * deltaTime;
       this.collider.checkY(object);
-      // this.collider.checkCollision(object, this.width, this.height);
     });
   }
 
@@ -56,12 +56,35 @@ export default class Game {
   getTile(x, y) {
     if (this.tileMap[x]) return this.tileMap[x][y];
   }
-  cameraView(camera, cb) {
-    this.tileMap.forEach((col, x) => {
-      col.forEach((tile, y) => {
-        cb(tile, x, y);
-      });
-    });
+  cameraView(camera, backgroundSpriteSheet, ctx) {
+    if (this.mario.pos.x > 300) {
+      camera.pos.x = this.mario.pos.x - 300;
+    }
+
+    const cameraPanel = document.createElement("canvas");
+    cameraPanel.width = camera.width + this.tileSize;
+    cameraPanel.height = camera.height;
+    const panelCtx = cameraPanel.getContext("2d");
+    const columnStart = this.getTileIndex(camera.pos.x);
+    const columnEnd = columnStart + this.getTileIndex(camera.width);
+
+    for (let x = columnStart; x <= columnEnd; x++) {
+      const column = this.tileMap[x];
+      if (column) {
+        column.forEach((tile, y) => {
+          backgroundSpriteSheet.draw(
+            tile.name,
+            panelCtx,
+            (x - columnStart) * this.tileSize,
+            y * this.tileSize
+          );
+        });
+      }
+    }
+    //draw camera by pixel for a smooth transition
+    ctx.drawImage(cameraPanel, -camera.pos.x % 29, -camera.pos.y);
   }
-  relevantTiles(start, end) {}
+  getTileIndex(pos) {
+    return Math.floor(pos / this.tileSize);
+  }
 }

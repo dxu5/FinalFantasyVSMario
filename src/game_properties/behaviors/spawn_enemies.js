26ba2ab709e710a2ebf4f0512e444a87c61c0332
dxu5 <derek_xu@brown.edu>
2020-11-04 08:16:04 -0800

    Add spawn enemies behavior

---
+++ src/game_properties/behaviors/spawn_enemies.js
@@ -0,0 +1,23 @@
+import Behavior from "./behavior";
+export default class SpawnEnemies extends Behavior {
+  constructor(gameObjects, enemies) {
+    super("spawnEnemies");
+    this.enemies = enemies;
+    this.gameObjects = gameObjects;
+  }
+
+  update(mario, deltaTime) {
+    this.enemies.forEach((enemy) => {
+      if (mario.pos.x >= enemy.trigger) {
+        this.spawnEnemy(enemy);
+      }
+    });
+  }
+  addEnemy(enemy) {
+    this.enemyPositions.add(enemy);
+  }
+  spawnEnemy(enemy) {
+    this.gameObjects.add(enemy);
+    this.enemies.delete(enemy);
+  }
+}
